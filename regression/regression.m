533325cea19781448889916c73346fe2c0f09b93
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-10-18 09:51:41 +0200

    Extend the import of the data Set
    - Remove index from dataset
    - Separate Features from Results
    - More descriptive message to conclude the importation

--- regression/regression.m
+++ regression/regression.m
@@ -1,6 +1,34 @@
+%% Control Variables
+filename = 'data/train.csv'; %File containing the Training Data
+rmIndexes = true; %Remove first column of the dataSet if contains the indexes
+normalization = true; %Activate or Desactivate the feature Normalization
 
-%% Import Training Datas
-filename = 'data/train.csv';
-data2Train = csvread(filename);
 
-disp('Training data imported successfully');
+%% Import Training Data
+% Import all the data set from the csv file
+dataSet = csvread(filename);
+
+% Remove the index from the dataSet
+if (rmIndexes)
+    dataSet = dataSet(:,2:end);
+end
+
+% Separate the Features from the Result
+[mDS,nDS] = size(dataSet); %Get the size of the data set matrix
+xDataSet = dataSet(:,1:(nDS - 1));
+yDataSet = dataSet(:,nDS);
+
+% Print the sucess message
+msg = ['Training data imported successfully.\n',...
+    ' --> #Features : ', int2str(nDS - 1),'\n',...
+    ' --> #Entries : ', int2str(mDS),'\n'];
+fprintf(msg);
+
+%% Feature Normalization
+if (normalization)
+    
+end
+
+%% Gradient Descent
+% http://www.codeproject.com/Articles/879043/Implementing-Gradient-Descent-to-Solve-a-Linear-Re
+
