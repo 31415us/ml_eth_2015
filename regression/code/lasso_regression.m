b6acdd80d28054fdf0a1cf41c7c549778220ff58
pius <piusvd@gmail.com>
2015-10-23 18:17:23 +0200

    update model

--- regression/code/lasso_regression.m
+++ regression/code/lasso_regression.m
@@ -1,26 +1,40 @@
 function [out] = lasso_regression(data_path, validation_path)
     id = @(x) x;
     inv = @(x) 1 ./ x;
+    square = @(x) x ./ x;
     
     transforms = cell(1, 15);
     transforms(:) = {id};
     transforms{1} = inv;
+    transforms{6} = inv;
     
     [X, mX, sX, Y, mY, sY] = load_data(data_path, transforms);
     
-    % 10 fold cross validated lasso
     [xrow, xcol] = size(X);
-    X = [ones(xrow, 1) X];
+    
+    % add squared columns
+    for i = 1:xcol
+        X = [X square(X(:,i))];
+    end
+    
+    % 10 fold cross validated lasso
     [B, stats] = lasso(X, Y, 'CV', 10);
     
+    disp(stats.MSE(stats.IndexMinMSE))
+    
     best_beta = B(:, stats.IndexMinMSE);
+    disp(best_beta(6))
+    best_intercept = stats.Intercept(stats.IndexMinMSE);
     
     [T, Indices] = load_test(validation_path, mX, sX, transforms);
     
-    [trow, tcol] = size(T);
-    T = [ones(trow, 1) T];
+    % add squared columns
+    for j = 1:xcol
+        T = [T square(T(:,j))];
+    end
     
-    out = T * best_beta;
+    
+    out = (T * best_beta) + best_intercept;
     out = (sY * out) + mY;
     
     out = [Indices out];
