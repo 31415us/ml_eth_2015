9518a66257a7844a5db1bb7f34ec4136911d42df
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-11-20 13:21:02 +0100

    Separate Transform function to be used in the master branch

---
+++ classification/code/transform.m
@@ -0,0 +1,15 @@
+function M = transform(X)
+%TRANSFORM is applying feature transformations
+%
+%   X     Matrix of data on which you want to perform feature transformations
+%
+%   M     Final Matrix with all the feature transformations
+
+    % Apply sqrt to the last column
+    M = [X(:,1:end-1) (sqrt(X(:, end)))];
+    
+    % Add new dimensions containing the square of the first columns
+    M = [M M(:,1:end-1).^2];
+    
+end
+
