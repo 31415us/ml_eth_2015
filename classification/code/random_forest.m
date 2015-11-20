6dc5b30e73bd541a6487beb07898f2c8e1e6b9a4
pius <piusvd@gmail.com>
2015-11-20 15:04:44 +0100

    remove hyperparameter search

--- classification/code/random_forest.m
+++ classification/code/random_forest.m
@@ -1,5 +1,5 @@
 function pred = random_forest(X, Y, T)
-    bagger = TreeBagger(1000, X, Y, 'OOBPrediction', 'on', 'Method', 'Classification', 'NumPredictorsToSample', 'all');
-    disp(mean(bagger.oobError));
+    bagger = TreeBagger(100, X, Y, 'Method', 'Classification', 'NumPredictorsToSample', 'all');
+    %disp(mean(bagger.oobError));
     pred = uint8(str2double(cell2mat(predict(bagger,T))));
 end
