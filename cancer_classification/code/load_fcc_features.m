83850c55e2ab3b3eacb3a968c53d50239ce3a8ca
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-12-18 18:42:26 +0100

    Fix minor bugs

--- cancer_classification/code/load_fcc_features.m
+++ cancer_classification/code/load_fcc_features.m
@@ -10,13 +10,13 @@ function fcc = load_fcc_features(img_ids, image_dir)
 
     [num_ids, ~] = size(img_ids);
     
-    fcc = zeros(num_ids, 1);
+    fcc = repmat(struct('x0', 0, 'y0', 0, 'code', 0), num_ids, 1 );
     
     for i = 1:num_ids
         msk_path = strcat(image_dir, sprintf('%04d', img_ids(i)), '_msk.png');
         
         msk = im2bw(imread(msk_path));
-        msk_perim = ~bwperim(msk, 1);
+        msk_perim = bwperim(msk);
         
         % Remove external line
         msk_perim(1,:) = 0;
@@ -24,13 +24,20 @@ function fcc = load_fcc_features(img_ids, image_dir)
         msk_perim(:,1) = 0;
         msk_perim(:,end) = 0;
         
+        % ----- DEBUG -----
+%         disp(msk_path);
+%         subplot(1,2,1), imshow(msk);
+%         subplot(1,2,2), imshow(msk_perim);
+%         pause;
+        
+        
         % TODO cell can separate in multiple smaller cell, find better way
         % to select cell
         [x_perim, y_perim] = find(msk_perim);
         
-        coord_perim = bwtraceboundary(msk_contour,[x_perim(1) y_perim(1)],'W',8,Inf,'counterclockwise');
+        coord_perim = bwtraceboundary(msk_perim,[x_perim(1) y_perim(1)],'W',8,Inf,'counterclockwise');
         
-        fcc(i,:) = chaincode(coord_perim);
+        fcc(i) = chaincode(coord_perim);
     end
 
 
