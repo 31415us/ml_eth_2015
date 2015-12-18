50757a6fd4a9083f88a6b2dfaf4bfcc2726f8c1b
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-12-18 18:13:18 +0100

    Comment some part of the file

--- cancer_classification/code/load_data.m
+++ cancer_classification/code/load_data.m
@@ -1,8 +1,14 @@
 function [X, Y] = load_data(train_path, image_prefix)
+% LOAD_DATA load all the different features from the training set         
+
     M = csvread(train_path);
     train_ids = M(:,1);
     Y = M(:,end);
+    
+    % Get the PHOG features and 1D signature from the design matrix
     PHOG_SIG = M(:,2:end-1);
+    
+    % Get the foreground and background color mean from the images
     FG_BG = load_fgbg_features(train_ids, image_prefix);
     
     X = [PHOG_SIG, FG_BG];
