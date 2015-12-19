809a31f871a056123c3e05b8aa7fee890dc3eac4
pius <piusvd@gmail.com>
2015-12-19 18:01:24 +0100

    store full kernel matrix in memory

--- cancer_classification/code/cv_svm_wrapper.m
+++ cancer_classification/code/cv_svm_wrapper.m
@@ -1,7 +1,7 @@
 function [cv_error] = cv_svm_wrapper(C, sigma, X, Y)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    model = fitcsvm(X, Y, 'BoxConstraint', C, 'KernelFunction', 'rbf', 'KernelScale', sigma, 'KFold', 10);
+    model = fitcsvm(X, Y, 'BoxConstraint', C, 'KernelFunction', 'rbf', 'KernelScale', sigma, 'KFold', 10, 'CacheSize', 'maximal');
     cv_error = model.kfoldLoss;
 end
 
--- cancer_classification/code/rbf_svm.m
+++ cancer_classification/code/rbf_svm.m
@@ -2,7 +2,7 @@ function pred = rbf_svm(X, Y, T)
 
     params = find_params(X, Y);
 
-    model = fitcsvm(X, Y, 'BoxConstraint', params(1), 'KernelFunction', 'rbf', 'KernelScale', params(2));
+    model = fitcsvm(X, Y, 'BoxConstraint', params(1), 'KernelFunction', 'rbf', 'KernelScale', params(2), 'CacheSize', 'maximal');
 
     pred = predict(model, T);
 end
